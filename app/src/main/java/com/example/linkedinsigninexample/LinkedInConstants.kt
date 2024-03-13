93718901f530713a313dfc985386eed8cfe9e395
CsAhmed2020 <csahmed.98.20@gmail.com>
2024-03-13 15:01:50 +0200

    c

--- app/src/main/java/com/example/linkedinsigninexample/LinkedInConstants.kt
+++ app/src/main/java/com/example/linkedinsigninexample/LinkedInConstants.kt
@@ -2,10 +2,10 @@ package com.example.linkedinsigninexample
 
 object LinkedInConstants {
 
-    val CLIENT_ID = "MY_CLIENT_ID"
-    val CLIENT_SECRET = "MY_CLIENT_SECRET"
-    val REDIRECT_URI = "MY_REDIRECT_URI"
-    val SCOPE = "r_liteprofile%20r_emailaddress"
+    val CLIENT_ID = "77ihcpvlaatr19"
+    val CLIENT_SECRET = "9W2AItNorkp58dra"
+    val REDIRECT_URI = "https://www.linkedin.com/developers/tools/oauth/redirect"
+    val SCOPE = "openid%20profile%20w_member_social%20email"
 
     val AUTHURL = "https://www.linkedin.com/oauth/v2/authorization"
     val TOKENURL = "https://www.linkedin.com/oauth/v2/accessToken"
